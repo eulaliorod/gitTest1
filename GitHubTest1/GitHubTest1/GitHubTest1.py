02c149d6a66899401e854a2e81497903bbd54970
eulaliorod <eulalio.rodriguez@gmail.com>
2017-09-26 18:36:10 -0600

    added code for newfeaature1

--- GitHubTest1/GitHubTest1/GitHubTest1.py
+++ GitHubTest1/GitHubTest1/GitHubTest1.py
@@ -1,4 +1,5 @@
-print('Hello git world'))
+print('Hello git world')
 #change made in github
 print('hello gitHub')
 print('hello visualstudio')
+#code to call feature 1
---
+++ GitHubTest1/GitHubTest1/newfeature1.py
@@ -0,0 +1,4 @@
+class newfeature1(object):
+    """description of class"""
+
+
